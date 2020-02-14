5ce4be6571c58e61072257a172d10b88fcf77508
pattonw <pattonw@hhmi.org>
2020-02-14 10:25:42 -0500

    flake8

--- neurolight_evaluation/fg_score.py
+++ neurolight_evaluation/fg_score.py
@@ -1,6 +1,7 @@
 import numpy as np
 import networkx as nx
 from sklearn.feature_extraction.image import grid_to_graph
+import sklearn
 from skimage.morphology import skeletonize as scikit_skeletonize
 
 from typing import Tuple
@@ -21,43 +22,43 @@ def skeletonize(
     binary_prediciton: np.ndarray, offset: np.ndarray, scale: np.ndarray
 ) -> nx.Graph:
     skeletonized_pred = scikit_skeletonize(binary_prediciton)
-    s = skeletonized_pred.shape
     # graph with nodes having voxel coordinates
-    skeleton_graph = grid_to_graph(skeleton=skeletonized_pred)
+    skeleton_graph = grid_to_nx_graph(skeleton=skeletonized_pred)
     # scaled into world units
     skeleton_graph = scale_skeleton(skeleton=skeleton_graph, offset=offset, scale=scale)
-    return skeleton
+    return skeleton_graph
 
 
 def scale_skeleton(
     skeleton: nx.Graph, offset: np.ndarray, scale: np.ndarray
-    ) -> nx.Graph:
+) -> nx.Graph:
     for n, data in skeleton.nodes():
-        voxel_loc = data['location']
+        voxel_loc = data["location"]
         space_loc = np.multiply(voxel_loc, scale) + offset
-        data['location'] = space_loc
+        data["location"] = space_loc
 
-    return skeleton       
+    return skeleton
 
 
-def grid_to_graph(
-    skeleton: np.ndarray
-    ) -> nx.Graph:
+def grid_to_nx_graph(skeleton: np.ndarray) -> nx.Graph:
     # Override with local function
     sklearn.feature_extraction.image._make_edges_3d = _make_edges_3d
 
     s = skeleton.shape
     # Identify connectivity
-    adj_mat = sklearn.feature_extraction.image.grid_to_graph(n_x=s[0], n_y=s[1], n_z=s[2], mask=skeleton)
+    adj_mat = grid_to_graph(n_x=s[0], n_y=s[1], n_z=s[2], mask=skeleton)
     # Identify order of the voxels
     voxel_locs = compute_voxel_locs(mask=skeleton)
 
     G = nx.Graph()
 
-    nodes = [(node_id, {'location': voxel_loc}) for node_id, voxel_loc in enumerate(voxel_locs)]
+    nodes = [
+        (node_id, {"location": voxel_loc})
+        for node_id, voxel_loc in enumerate(voxel_locs)
+    ]
     G.add_nodes_from(nodes)
 
-    edges = [(a, b) for a, b in zip(adj_mat.row adj_mat.col)]
+    edges = [(a, b) for a, b in zip(adj_mat.row, adj_mat.col)]
     G.add_edges_from(edges)
 
     return G
@@ -66,9 +67,7 @@ def grid_to_graph(
 # From https://github.com/neurodata/scikit-learn/blob/tom/grid_to_graph_26/sklearn/feature_extraction/image.py
 # Used in grid_to_graph
 # automatically set to 26-connectivity
-def _make_edges_3d(
-    n_x: int, n_y: int, n_z: int, connectivity=26
-    ):
+def _make_edges_3d(n_x: int, n_y: int, n_z: int, connectivity=26):
     """Returns a list of edges for a 3D image.
     Parameters
     ----------
@@ -88,52 +87,73 @@ def _make_edges_3d(
 
     edges = []
 
-    edges_deep = np.vstack((vertices[:, :, :-1].ravel(),
-                            vertices[:, :, 1:].ravel()))
-    edges_right = np.vstack((vertices[:, :-1].ravel(),
-                             vertices[:, 1:].ravel()))
+    edges_deep = np.vstack((vertices[:, :, :-1].ravel(), vertices[:, :, 1:].ravel()))
+    edges_right = np.vstack((vertices[:, :-1].ravel(), vertices[:, 1:].ravel()))
     edges_down = np.vstack((vertices[:-1].ravel(), vertices[1:].ravel()))
 
     edges = [edges_deep, edges_right, edges_down]
 
     # Add the other connections
     if connectivity >= 18:
-        edges_right_deep = np.vstack((vertices[:, :-1, :-1].ravel(),
-                                      vertices[:, 1:, 1:].ravel()))
-        edges_down_right = np.vstack((vertices[:-1, :-1, :].ravel(),
-                                      vertices[1:, 1:, :].ravel()))
-        edges_down_deep = np.vstack((vertices[:-1, :, :-1].ravel(),
-                                     vertices[1:, :, 1:].ravel()))
-        edges_down_left = np.vstack((vertices[:-1, 1:, :].ravel(),
-                                     vertices[1:, :-1, :].ravel()))
-        edges_down_shallow = np.vstack((vertices[:-1, :, 1:].ravel(),
-                                        vertices[1:, :, :-1].ravel()))
-        edges_deep_left = np.vstack((vertices[:, 1:, :-1].ravel(),
-                                     vertices[:, :-1, 1:].ravel()))
+        edges_right_deep = np.vstack(
+            (vertices[:, :-1, :-1].ravel(), vertices[:, 1:, 1:].ravel())
+        )
+        edges_down_right = np.vstack(
+            (vertices[:-1, :-1, :].ravel(), vertices[1:, 1:, :].ravel())
+        )
+        edges_down_deep = np.vstack(
+            (vertices[:-1, :, :-1].ravel(), vertices[1:, :, 1:].ravel())
+        )
+        edges_down_left = np.vstack(
+            (vertices[:-1, 1:, :].ravel(), vertices[1:, :-1, :].ravel())
+        )
+        edges_down_shallow = np.vstack(
+            (vertices[:-1, :, 1:].ravel(), vertices[1:, :, :-1].ravel())
+        )
+        edges_deep_left = np.vstack(
+            (vertices[:, 1:, :-1].ravel(), vertices[:, :-1, 1:].ravel())
+        )
 
-        edges.extend([edges_right_deep, edges_down_right, edges_down_deep,
-                      edges_down_left, edges_down_shallow, edges_deep_left])
+        edges.extend(
+            [
+                edges_right_deep,
+                edges_down_right,
+                edges_down_deep,
+                edges_down_left,
+                edges_down_shallow,
+                edges_deep_left,
+            ]
+        )
 
     if connectivity == 26:
-        edges_down_right_deep = np.vstack((vertices[:-1, :-1, :-1].ravel(),
-                                           vertices[1:, 1:, 1:].ravel()))
-        edges_down_left_deep = np.vstack((vertices[:-1, 1:, :-1].ravel(),
-                                          vertices[1:, :-1, 1:].ravel()))
-        edges_down_right_shallow = np.vstack((vertices[:-1, :-1, 1:].ravel(),
-                                              vertices[1:, 1:, :-1].ravel()))
-        edges_down_left_shallow = np.vstack((vertices[:-1, 1:, 1:].ravel(),
-                                             vertices[1:, :-1, :-1].ravel()))
+        edges_down_right_deep = np.vstack(
+            (vertices[:-1, :-1, :-1].ravel(), vertices[1:, 1:, 1:].ravel())
+        )
+        edges_down_left_deep = np.vstack(
+            (vertices[:-1, 1:, :-1].ravel(), vertices[1:, :-1, 1:].ravel())
+        )
+        edges_down_right_shallow = np.vstack(
+            (vertices[:-1, :-1, 1:].ravel(), vertices[1:, 1:, :-1].ravel())
+        )
+        edges_down_left_shallow = np.vstack(
+            (vertices[:-1, 1:, 1:].ravel(), vertices[1:, :-1, :-1].ravel())
+        )
 
-        edges.extend([edges_down_right_deep, edges_down_left_deep,
-                      edges_down_right_shallow, edges_down_left_shallow])
+        edges.extend(
+            [
+                edges_down_right_deep,
+                edges_down_left_deep,
+                edges_down_right_shallow,
+                edges_down_left_shallow,
+            ]
+        )
 
     edges = np.hstack(edges)
     return edges
 
-def compute_voxel_locs(
-    mask: np.ndarray
-    ) -> np.ndarray:
-    
+
+def compute_voxel_locs(mask: np.ndarray) -> np.ndarray:
+
     locs = np.where(mask == 1)
     locs = np.stack(locs, axis=1)
     return locs
