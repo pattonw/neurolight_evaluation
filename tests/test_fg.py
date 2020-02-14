f239bf81a627b4552d58ed93b43da8a3237da679
Thomas Athey <tom.l.athey@gmail.com>
2020-02-14 14:04:35 -0500

    simple match test

---
+++ tests/test_fg.py
@@ -0,0 +1,20 @@
+import numpy as np
+import networkx as nx
+
+from neurolight_evaluation import score_foreground
+
+def test_simple_match():
+    offset = np.array([0,0,0])
+    scale = np.array([1,1,1])
+
+    pred = np.stack((np.eye(10), np.zeros([10,10])), axis=2)
+
+    ref = nx.Graph()
+    a = np.array([0,0,0])
+    b = np.array([9,9,0])
+    ref.add_nodes_from([('a',{'loc':a}),('b',{'loc':b})])
+    ref.add_edge('a','b')
+
+    recall, precision = score_foreground(
+        binary_prediction=pred, reference_tracings=ref,
+        offset=offset, scale=scale)
