c1e3b74b83b3cd520b09e123b651c90ba3f194ca
pattonw <pattonw@hhmi.org>
2020-04-30 08:48:28 -0400

    add a psuedo graph-edit distance cost

--- neurolight_evaluation/common.py
+++ neurolight_evaluation/common.py
@@ -113,3 +113,119 @@ def recall_precision(
         precision = matched_y / (total_y + 1e-4)
 
     return recall, precision
+
+
+def psudo_graph_edit_distance(
+    node_matchings: List[Tuple[int, int]],
+    node_x_labels: Dict[int, int],
+    node_y_labels: Dict[int, int],
+    graph_x: nx.Graph,
+    graph_y: nx.Graph,
+    location_attr: str,
+    node_spacing: float,
+) -> Tuple[float, float]:
+    """
+    Calculate a psuedo graph edit distance.
+    
+    The goal of this metric is to approximate the amount of time
+    it would take a trained tracing expert to correct a predicted
+    graph.
+
+    An edge (a, b) needs to be removed or added if a matches to some label
+    not equal to the label that b matches to. Removing this edge
+    should be 1 click, and thus contributes penalty of 1 to this metric.
+    This covers Splits and Merges
+
+    Every connected component of false positives should be removed
+    with 1 click so they each contribute 1.
+
+    Every node in y that matches to None, must be reconstructed.
+    The time it takes to reconstruct false negatives is based on
+    cable length. Summing the total cable length adjacent to a
+    false negative node, and dividing by two, gives us an approximation
+    of false negative cable length. Dividing by 5 microns gives an
+    approximation of how many nodes will need to be added. This is
+    the weight of a false negative node.
+
+    Args:
+
+        node_matchings: (``list`` of ``tuple`` pairs of ``int``)
+    
+            A list of tuples containing pairs of nodes that match
+
+        node_x_labels: (``dict`` mapping ``int`` to ``int``)
+
+            A dictionary mapping node_ids in graph_x (assumed to be integers)
+            to label ids in graph_x (also assumed to be integers)
+
+        node_y_labels: (``dict`` mapping ``int`` to ``int``)
+
+            A dictionary mapping node_ids in graph_y (assumed to be integers)
+            to label ids in graph_y (also assumed to be integers)
+
+        graph_x: (``nx.Graph``)
+
+            The graph_x on which to calculate recall
+
+        graph_y: (``nx.Graph``)
+
+            the graph_y on which to calculate precision
+
+        location_attr: (``str``)
+
+            An attribute that all nodes in graph_x and graph_y have that contains
+            a node's location for calculating edge lengths.
+
+    Returns:
+
+        (``float``):
+
+            cost of this matching
+    """
+
+    nomatch_node = max(list(graph_x.nodes) + list(graph_y.nodes)) + 1
+    nomatch_label = max(list(node_x_labels.values()) + list(node_y_labels.values())) + 1
+
+    x_node_to_y_label = {}
+    y_node_to_x_label = {}
+    for a, b in node_matchings:
+        y_label = x_node_to_y_label.setdefault(a, node_y_labels[b])
+        assert y_label == node_y_labels[b], (
+            f"node {a} in graph_x matches to multiple labels in graph_y, "
+            f"including {(y_label, node_y_labels[b])}!"
+        )
+        x_label = y_node_to_x_label.setdefault(b, node_x_labels[a])
+        assert x_label == node_x_labels[a], (
+            f"node {b} in graph_y matches to multiple labels in graph_x, "
+            f"including {(x_label, node_x_labels[a])}!"
+        )
+
+    false_pos_nodes = [
+        x_node for x_node, y_node in node_matchings if y_node == nomatch_node
+    ]
+    false_neg_nodes = [
+        y_node for x_node, y_node in node_matchings if x_node == nomatch_node
+    ]
+
+    false_pos_cost = len(
+        list(nx.connected_components(graph_x.subgraph(false_pos_nodes)))
+    )
+    false_neg_cost = 0
+    for node in false_neg_nodes:
+        cable_len = 0
+        for neighbor in graph_y.neighbors(node):
+            node_loc = graph_y.nodes[node][location_attr]
+            neighbor_loc = graph_y.nodes[neighbor][location_attr]
+            cable_len += np.linalg.norm(node_loc - neighbor_loc) / 2
+        false_neg_cost += cable_len / node_spacing
+
+    merge_cost = 0
+    for u, v in graph_x.edges:
+        if x_node_to_y_label[u] != x_node_to_y_label[v]:
+            merge_cost += 1
+    split_cost = 0
+    for u, v in graph_y.edges:
+        if y_node_to_x_label[u] != y_node_to_x_label[v]:
+            split_cost += 1
+
+    return false_pos_cost + false_neg_cost + merge_cost + split_cost
